cdf1ec4a48755f2ad0e3a54a41079eb6835c43f7
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-09-03 15:53:37 -0500

    assests load y crear funcion

---
+++ code/assets.js
@@ -0,0 +1,16 @@
+export default function loadAssets() {
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("ghosty", "sprites/ghosty.png");
+	loadSprite("spike", "sprites/spike.png");
+	loadSprite("grass", "sprites/grass.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png");
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("coin", "sprites/coin.png");
+  	loadSprite("dino", "/sprites/dino.png");
+	loadSound("coin", "sounds/score.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/hit.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+}
