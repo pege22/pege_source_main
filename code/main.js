bb138c40283365e6df8bea56a810b5f3541d66fc
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-09-03 14:32:31 -0500

    Codigo principal actualizado

---
+++ code/main.js
@@ -0,0 +1,97 @@
+//CodedByLuis
+import kaboom from "kaboom"
+
+kaboom({
+
+	scale: 6,  
+
+	font: "sinko",
+})
+
+
+loadSprite("dino", "/sprites/dino.png", {
+
+	sliceX: 9,
+
+	anims: {
+		"idle": {
+
+			from: 0,
+			to: 3,
+
+			speed: 2,
+			loop: true,
+		},
+		"run": {
+			from: 4,
+			to: 7,
+			speed: 10,
+			loop: true,
+		},
+
+		"jump": 8
+	},
+})
+
+const SPEED = 50
+const JUMP_FORCE = 240
+
+gravity(640)
+
+
+const player = add([
+	sprite("dino"),
+	pos(center()),
+	origin("center"),
+	area(),
+	body(),
+])
+
+
+player.play("idle")
+
+
+
+
+
+player.onGround(() => {
+	if (!isKeyDown("left") && !isKeyDown("right")) {
+		player.play("idle")
+	} else {
+		player.play("run")
+	}
+})
+
+player.onAnimEnd("idle", () => {
+
+})
+
+onKeyPress("space", () => {
+	if (player.isGrounded()) {
+		player.jump(JUMP_FORCE)
+		player.play("jump")
+	}
+})
+
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+	player.flipX(true)
+
+	if (player.isGrounded() && player.curAnim() !== "run") {
+		player.play("run")
+	}
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+	player.flipX(false)
+	if (player.isGrounded() && player.curAnim() !== "run") {
+		player.play("run")
+	}
+})
+
+onKeyRelease(["left", "right"], () => {
+	if (player.isGrounded() && !isKeyDown("left") && !isKeyDown("right")) {
+		player.play("idle")
+	}
+})
