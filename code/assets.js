fde9fb30ef5879ea78ec753bc490293ed7c68353
JeroUwU <111304128+JeroUwU@users.noreply.github.com>
2022-09-04 13:43:50 -0500

    spikes
    sdfcgvmk

--- code/assets.js
+++ code/assets.js
@@ -1,6 +1,6 @@
 export default function loadAssets() {
 	loadSprite("ghosty", "sprites/ghosty.png");
-	loadSprite("spike", "sprites/spikes_Animation 1_0.png");
+	loadSprite("spike", "sprites/spikes2_Animation 1_0.png");
 	loadSprite("grass", "sprites/grass2_Animation 1_0.png");
 	loadSprite("prize", "sprites/jumpy.png");
 	loadSprite("apple", "sprites/apple.png");
