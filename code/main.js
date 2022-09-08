c4cbe01685d3dd28b6ee56cf5862db652b0558ac
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-09-07 20:56:03 -0500

    Fixed imports

--- code/main.js
+++ code/main.js
@@ -1,5 +1,22 @@
 import kaboom from "kaboom"
-import loadAssets from "./assets"
+
+//load sprites and sounds
+	loadSprite("ghosty", "sprites/ghosty.png");
+	loadSprite("spike", "sprites/spikes2_Animation 1_0.png");
+	loadSprite("grass", "sprites/grass2_Animation 1_0.png");
+	loadSprite("piedra", "sprites/piedra.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png"); //fix apple
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("npc", "sprites/p1fix.png");
+	loadSprite("coin", "sprites/moneda 4_MONEDA_0.png");
+  	loadSprite("dino", "/sprites/dino.png");
+	loadSound("coin", "sounds/score.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/hit.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+//Fixed assets load
 //CodedByLuis
 //CompetPanas
 //#LuisNet
