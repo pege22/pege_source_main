b2ec5398a2beb8ba6348f9088eaca593c94eb515
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-09-29 21:46:26 -0500

    big update

--- code/assets.js
+++ code/assets.js
@@ -3,15 +3,17 @@ export default function loadAssets() {
 	loadSprite("spike", "sprites/spikes2_Animation 1_0.png");
 	loadSprite("grass", "sprites/grass2_Animation 1_0.png");
 	loadSprite("piedra", "sprites/piedra.png");
+  loadSprite("mafe", "sprites/Mafe.png.png");
 	loadSprite("prize", "sprites/jumpy.png");
+  	loadSprite("gem", "sprites/gem.png");
   loadSprite("bg", "sprites/descarga.png");
   loadSprite("menubg", "sprites/bg2.png");
 	loadSprite("apple", "sprites/apple.png"); //fix apple
-	loadSprite("portal", "sprites/portal.png");
+	loadSprite("portal", "sprites/portal3.png");
 	loadSprite("npc", "sprites/p1fix.png");
 	loadSprite("coin", "sprites/moneda 4_MONEDA_0.png");
   	loadSprite("dino", "/sprites/dino.png");
-	loadSprite("spikevoltiado", "/sprites/spikes turned_Animation 1_0.png");
+	loadSprite("spikevoltiado", "sprites/spikes turned_Animation 1_0.png");
 	loadSound("coin", "sounds/score.mp3");
 	loadSound("powerup", "sounds/powerup.mp3");
 	loadSound("blip", "sounds/blip.mp3");
