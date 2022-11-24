ae6844477a58936fa65695f13a9e6496bc8200db
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-24 13:11:41 -0500

    Beautify

--- code/assets.js
+++ code/assets.js
@@ -1,13 +1,13 @@
 export default function loadAssets() {
-	loadSprite("ghosty", "sprites/GHOSTY_ENEMIES_0.png");
+  loadSprite("ghosty", "sprites/GHOSTY_ENEMIES_0.png");
   loadSprite("forest1", "sprites/Mosscreep.png");
   loadSprite("ruinasbg", "sprites/ruinasbg.png");
   loadSprite("forestbg", "sprites/bosque2bg.png")
   loadSprite("ciudadbg", "sprites/ciudadbg.png")
   loadSprite("icebg", "sprites/icebg.png")
-	loadSprite("spike", "sprites/spikes2_Animation 1_0.png");
-	loadSprite("grass", "sprites/grass2_Animation 1_0.png");
-	loadSprite("piedra", "sprites/piedra.png");
+  loadSprite("spike", "sprites/spikes2_Animation 1_0.png");
+  loadSprite("grass", "sprites/grass2_Animation 1_0.png");
+  loadSprite("piedra", "sprites/piedra.png");
   loadSprite("piedra", "sprites/piedra.png");
   loadSprite("wood", "sprites/Wood_BLOCKS_0.png");
   loadSprite("mountain", "sprites/Mountain.png");
@@ -21,7 +21,7 @@ export default function loadAssets() {
   loadSprite("silvi", "sprites/Personajes_Silvi.png");
   loadSprite("valeuwu", "sprites/Personajes_ValeQ.png");
   loadSprite("july", "sprites/Personajes_July.png");
-	loadSprite("prize", "sprites/jumpy.png");
+  loadSprite("prize", "sprites/jumpy.png");
   loadSprite("gemp1", "sprites/gemp1.png");
   loadSprite("gemp2", "sprites/gemp2.png");
   loadSprite("gemp3", "sprites/gemp3.png");
@@ -30,14 +30,14 @@ export default function loadAssets() {
   loadSprite("gem", "sprites/gem.png");
   loadSprite("bfuerabg", "sprites/descarga.png");
   loadSprite("menubg", "sprites/manubg2.gif");
-	loadSprite("apple", "sprites/apple.png"); 
-	loadSprite("portal", "sprites/portal3.png");
-	loadSprite("coin", "sprites/monedaa.png");
-	loadSprite("spikevoltiado", "sprites/spikes turned_Animation 1_0.png");
-	loadSound("coin", "sounds/score.mp3");
-	loadSound("powerup", "sounds/powerup.mp3");
-	loadSound("blip", "sounds/blip.mp3");
-	loadSound("hit", "sounds/hit.mp3");
+  loadSprite("apple", "sprites/apple.png"); 
+  loadSprite("portal", "sprites/portal3.png");
+  loadSprite("coin", "sprites/monedaa.png");
+  loadSprite("spikevoltiado", "sprites/spikes turned_Animation 1_0.png");
+  loadSound("coin", "sounds/score.mp3");
+  loadSound("powerup", "sounds/powerup.mp3");
+  loadSound("blip", "sounds/blip.mp3");
+  loadSound("hit", "sounds/hit.mp3");
   loadSound("yei", "sounds/congrat2.mp3");
-	loadSound("portal", "sounds/portal.mp3");
+  loadSound("portal", "sounds/portal.mp3");
 }
