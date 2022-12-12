9758cfa844005918660753e09162141baf7cd060
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-12-11 20:52:17 -0500

    Update huge

--- start.sh
+++ start.sh
@@ -1,39 +1,41 @@
-echo Iniciando..
+echo Starting...
 sleep 1.5
-cd Site
 sleep 1
+BLUE="\e[34m"
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
-
-
-echo -e "${RED} ██▓     █    ██  ██▓  ██████  ███▄    █ ▓█████▄▄▄█████▓";
-echo -e "${RED}▓██▒     ██  ▓██▒▓██▒▒██    ▒  ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒";
-echo -e "${RED}▒██░    ▓██  ▒██░▒██▒░ ▓██▄   ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░";
-echo -e "${RED}▒██░    ▓▓█  ░██░░██░  ▒   ██▒▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░ ";
-echo -e "${RED}░██████▒▒▒█████▓ ░██░▒██████▒▒▒██░   ▓██░░▒████▒ ▒██▒ ░ ";
-echo -e "${RED}░ ▒░▓  ░░▒▓▒ ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░   ";
-echo -e "${RED}░ ░ ▒  ░░░▒░ ░ ░  ▒ ░░ ░▒  ░ ░░ ░░   ░ ▒░ ░ ░  ░   ░    ";
-echo -e "${RED}  ░ ░    ░░░ ░ ░  ▒ ░░  ░  ░     ░   ░ ░    ░    ░      ";
-echo -e "${RED}    ░  ░   ░      ░        ░           ░    ░  ░        ";
-echo -e "${RED}                                                        ";
+echo -e "${RED} ██▓     █    ██  ██▓  ██████  ███▄    █ ▓█████▄▄▄█████▓${ENDCOLOR}";
+echo -e "${RED}▓██▒     ██  ▓██▒▓██▒▒██    ▒  ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒${ENDCOLOR}";
+echo -e "${RED}▒██░    ▓██  ▒██░▒██▒░ ▓██▄   ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░${ENDCOLOR}";
+echo -e "${RED}▒██░    ▓▓█  ░██░░██░  ▒   ██▒▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░ ${ENDCOLOR}";
+echo -e "${RED}░██████▒▒▒█████▓ ░██░▒██████▒▒▒██░   ▓██░░▒████▒ ▒██▒ ░ ${ENDCOLOR}";
+echo -e "${RED}░ ▒░▓  ░░▒▓▒ ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░   ${ENDCOLOR}";
+echo -e "${RED}░ ░ ▒  ░░░▒░ ░ ░  ▒ ░░ ░▒  ░ ░░ ░░   ░ ▒░ ░ ░  ░   ░    ${ENDCOLOR}";
+echo -e "${RED}  ░ ░    ░░░ ░ ░  ▒ ░░  ░  ░     ░   ░ ░    ░    ░      ${ENDCOLOR}";
+echo -e "${RED}    ░  ░   ░      ░        ░           ░    ░  ░        ${ENDCOLOR}";
+echo -e "${RED}                                                        ${ENDCOLOR}";
 sleep 5
+echo -e "${GREEN} Looking for folder...${ENDCOLOR}"
+sleep 2
+echo "Created by: Luis9799 for Southestearn Lousiana University PEGE'S FAIR 2022"
+echo "Copyright all rights reserved 2022, MIT LICENSE"
+echo -e "You're free to use this software for your own, you can ${RED}NOT${ENDCOLOR} distribute it by your name and generate income, otherwise, federal law will be applied."
+echo -e "${BLUE}Twitter: @yt1luiscraft${ENDCOLOR}"
+echo -e "${BLUE} Discord: Luis!#5902 ${ENDCOLOR}"
+echo -e "${BLUE}Telegram: @L9799${ENDCOLOR}"
+echo "Web: https://luisweb.cf"
+sleep 3
+echo Starting webserver...
+sleep 1
+echo WebServer iniciado en el puerto 8080
+sleep 2
 clear
-echo Backend Creado por Luis9799 
-echo Twitter: yt1luiscraft
-echo Discord: Luis!#5902
-echo Github: LUIS9799
-echo Telegram: L9799
-echo Correo: soporte@luisweb.cf
-echo       BACKEND SH FOR PEGE PROYECT
 sleep 1
-echo Iniciando webserver..
-sleep 1
-echo WebServer iniciado en el puerto 8080 y websocket en el puerto 3000 
 #Heroku fix by luis
 sleep 2
 npm install
-sleep 2
+sleep 1
 clear
 sleep 1
 node run.js
